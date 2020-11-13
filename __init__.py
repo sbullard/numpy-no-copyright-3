ec4db1edfca536c7cf28b8d5ee98bad47af4a2f1
sbullard <69687511+sbullard@users.noreply.github.com>
2020-11-13 12:56:04 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,117 +1,62 @@
-#!/usr/bin/env python3
-"""Fortran to Python Interface Generator.
+"""
+**Note:** almost all functions in the ``numpy.lib`` namespace
+are also present in the main ``numpy`` namespace.  Please use the
+functions as ``np.<funcname>`` where possible.
+
+``numpy.lib`` is mostly a space for implementing functions that don't
+belong in core or in another NumPy submodule with a clear purpose
+(e.g. ``random``, ``fft``, ``linalg``, ``ma``).
+
+Most contains basic functions that are used by several submodules and are
+useful to have in the main name-space.
 
 """
-__all__ = ['run_main', 'compile', 'f2py_testing']
+import math
 
-import sys
-import subprocess
-import os
+from numpy.version import version as __version__
 
-from . import f2py2e
-from . import f2py_testing
-from . import diagnose
+# Public submodules
+# Note: recfunctions and (maybe) format are public too, but not imported
+from . import mixins
+from . import scimath as emath
 
-run_main = f2py2e.run_main
-main = f2py2e.main
+# Private submodules
+from .type_check import *
+from .index_tricks import *
+from .function_base import *
+from .nanfunctions import *
+from .shape_base import *
+from .stride_tricks import *
+from .twodim_base import *
+from .ufunclike import *
+from .histograms import *
 
+from .polynomial import *
+from .utils import *
+from .arraysetops import *
+from .npyio import *
+from .financial import *
+from .arrayterator import Arrayterator
+from .arraypad import *
+from ._version import *
+from numpy.core._multiarray_umath import tracemalloc_domain
 
-def compile(source,
-            modulename='untitled',
-            extra_args='',
-            verbose=True,
-            source_fn=None,
-            extension='.f'
-           ):
-    """
-    Build extension module from a Fortran 77 source string with f2py.
-
-    Parameters
-    ----------
-    source : str or bytes
-        Fortran source of module / subroutine to compile
-
-        .. versionchanged:: 1.16.0
-           Accept str as well as bytes
-
-    modulename : str, optional
-        The name of the compiled python module
-    extra_args : str or list, optional
-        Additional parameters passed to f2py
-
-        .. versionchanged:: 1.16.0
-            A list of args may also be provided.
-
-    verbose : bool, optional
-        Print f2py output to screen
-    source_fn : str, optional
-        Name of the file where the fortran source is written.
-        The default is to use a temporary file with the extension
-        provided by the `extension` parameter
-    extension : {'.f', '.f90'}, optional
-        Filename extension if `source_fn` is not provided.
-        The extension tells which fortran standard is used.
-        The default is `.f`, which implies F77 standard.
-
-        .. versionadded:: 1.11.0
-
-    Returns
-    -------
-    result : int
-        0 on success
-
-    Examples
-    --------
-    .. include:: compile_session.dat
-        :literal:
-
-    """
-    import tempfile
-    import shlex
-
-    if source_fn is None:
-        f, fname = tempfile.mkstemp(suffix=extension)
-        # f is a file descriptor so need to close it
-        # carefully -- not with .close() directly
-        os.close(f)
-    else:
-        fname = source_fn
-
-    if not isinstance(source, str):
-        source = str(source, 'utf-8')
-    try:
-        with open(fname, 'w') as f:
-            f.write(source)
-
-        args = ['-c', '-m', modulename, f.name]
-
-        if isinstance(extra_args, str):
-            is_posix = (os.name == 'posix')
-            extra_args = shlex.split(extra_args, posix=is_posix)
-
-        args.extend(extra_args)
-
-        c = [sys.executable,
-             '-c',
-             'import numpy.f2py as f2py2e;f2py2e.main()'] + args
-        try:
-            output = subprocess.check_output(c)
-        except subprocess.CalledProcessError as exc:
-            status = exc.returncode
-            output = ''
-        except OSError:
-            # preserve historic status code used by exec_command()
-            status = 127
-            output = ''
-        else:
-            status = 0
-            output = output.decode()
-        if verbose:
-            print(output)
-    finally:
-        if source_fn is None:
-            os.remove(fname)
-    return status
+__all__ = ['emath', 'math', 'tracemalloc_domain', 'Arrayterator']
+__all__ += type_check.__all__
+__all__ += index_tricks.__all__
+__all__ += function_base.__all__
+__all__ += shape_base.__all__
+__all__ += stride_tricks.__all__
+__all__ += twodim_base.__all__
+__all__ += ufunclike.__all__
+__all__ += arraypad.__all__
+__all__ += polynomial.__all__
+__all__ += utils.__all__
+__all__ += arraysetops.__all__
+__all__ += npyio.__all__
+__all__ += financial.__all__
+__all__ += nanfunctions.__all__
+__all__ += histograms.__all__
 
 from numpy._pytesttester import PytestTester
 test = PytestTester(__name__)
